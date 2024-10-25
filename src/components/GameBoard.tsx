d4bf7a2e245f22f4949b26ae8c97cd6e98ad6701
falamarcao <falamarcao@gmail.com>
2024-10-25 04:11:59 -0300

    Disabling Buttons Conditionally

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -33,7 +33,10 @@ const GameBoard = ({ onSelectSquare, turns }: GameBoardProps) => {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
